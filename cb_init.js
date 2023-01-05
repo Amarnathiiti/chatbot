8b02d5b742fb8619cf448b7bce0847f08c5df167
Amarnath Kumar <69647253+Amarnathiiti@users.noreply.github.com>
2023-01-05 13:43:34 +0530

    Update cb_init.js

--- cb_init.js
+++ cb_init.js
@@ -28,7 +28,7 @@ div.innerHTML = `<div class="container">
         </div>
     </div>
     <div class="chatbox__button">
-        <button><img src=src="https://cdn-icons-png.flaticon.com/512/8943/8943377.png" width="100px" /></button>
+        <button><img src="https://cdn-icons-png.flaticon.com/512/8943/8943377.png" width="100px" /></button>
     </div>
 </div>
 </div>`
